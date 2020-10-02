b28ca81313e7fc08d16f22aa442a06d808d0f982
Yeraldina <67125251+yestrella14@users.noreply.github.com>
2020-10-01 22:49:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.graph_objs as go
 beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
 ibu_values=[35, 60, 85, 75]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='fucshia'
+color1='gold'
 color2='royalblue'
 mytitle='Beer Comparison'
 tabtitle='beer!'
